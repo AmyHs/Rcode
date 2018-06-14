80eab576aeb69193c1e1de4d3082ce4fb3dcc54a
AmyHs <36941267+AmyHs@users.noreply.github.com>
2018-06-14 21:46:25 +0800

    project3 upload

--- week_8_project3_Titanic/titanic.R
+++ week_8_project3_Titanic/titanic.R
@@ -1,120 +1,73 @@
-#資料
-library(tidyverse)
-setwd("C:/Users/b0520/Desktop/csx_R_course/106-2RSampleCode/week_8/task_8")
-train<-read.csv("titanicTrain.csv")
-train<-train[-c(1001:1310),]
+raw_data<-read.csv("titanicTrain.csv")
+raw_data<-raw_data[-c(1001:1310),]
 quest<-read.csv("titanicQuestion.csv")
-all<-rbind(train,train)
-#資料特徵
-str(train)
-
-#資料特徵缺失值
-is_na<-function(x){
-  sum(is.na(x))
-}
-sapply(train,is_na)
-sapply(quest,is_na)
-sapply(all,is_na)#怎麼fare變成0缺失
-quest[is.na(quest$fare)==1,]
-all[all$name=="Storey, Mr. Thomas",]
-
-#資料探索
-##0.1資料轉換
-train$survived<-as.factor(train$survived)
-train$pclass<-as.ordered(train$pclass)
-##0.2死亡存活數
-die_survived<-ggplot(train[!is.na(train$survived),],aes(x=survived))+geom_bar(stat="count")+labs(x="How many people die")+geom_label(stat = "count",aes (label= ..count..))
-##0.3性別與死亡
-sex_die<-ggplot(train[!is.na(train$survived),], aes(x = sex, fill = survived)) +
-  geom_bar(stat='count', position='dodge') + theme_grey() +
-  labs(x = 'Training data only') +
-  geom_label(stat='count', aes(label=..count..))
-##0.4死亡與pclass
-###0.4.1 class與死亡
-pclass_die<-ggplot(train[!is.na(train$survived),], aes(x = pclass, fill = survived)) +
-  geom_bar(stat='count', position='dodge') + labs(x = 'Training data only') +
-  theme(legend.position="none") 
-###0.4.2 class& sex &死亡
-pclass_sex_die1<-ggplot(train[!is.na(train$survived),], aes(x = pclass, fill = survived)) +
-  geom_bar(stat='count', position='fill') +
-  labs(x = 'Training data only', y= "Percent") + facet_grid(.~sex) 
-pclass_sex_die2<-ggplot(train[!is.na(train$survived),], aes(x = pclass, fill = survived)) +
-  geom_bar(stat='count', position='stack') +
-  labs(x = 'Training data only', y= "Count") + facet_grid(.~sex)
-###0.4.3把pclass & sex做組合
-p1f<-which((train$pclass == "1") & (train$sex == "female"))
-p2f<-which((train$pclass == "2") & (train$sex == "female"))
-p3f<-which((train$pclass == "3") & (train$sex == "female"))
-p1m<-which((train$pclass == "1") & (train$sex == "male"))
-p2m<-which((train$pclass == "2") & (train$sex == "male"))
-p3m<-which((train$pclass == "3") & (train$sex == "male"))
-train$pclass_sex[p1f]="p1f"
-train$pclass_sex[p2f]="p2f"
-train$pclass_sex[p3f]="p3f"
-train$pclass_sex[p1m]="p1m"
-train$pclass_sex[p2m]="p2m"
-train$pclass_sex[p3m]="p3m"
-as.factor(train$pclass_sex)
-
-
-##0.5 fare&死亡
-train[which(train$fare==0),]#有class1&大人是fare=0
-pclass_fare<-ggplot(train[!is.na(train$survived),], aes(x = fare, fill = survived)) +
-  geom_histogram( bins= 30) +
-  labs(x = 'Training data only', y= "Count") + facet_grid(.~pclass)
-###看不出什麼結果
-
-##0.6 年紀與死亡
-###0.6.1先解決boat的缺失值
-train$boat = as.numeric(train$boat != "")
-###0.6.2用現有資料預測年紀的缺失值
-agelm <- lm(age ~ pclass+boat+sex+parch+sibsp, data=train[!is.na(train$age),])
-summary(agelm)
-train$agelm<-predict(agelm,train)
-###0.6.3預測結果
-hist(train$age[!is.na(train$age)], main='Original data, non-missing', xlab='age')
-hist(train$agelm, main= 'LM predictions', xlab='age', xlim=range(0:80))
-###0.6.4把現有資料跟預測結果作合併
-indexMissingAge <- which(is.na(train$age))
-train$age[indexMissingAge] <- train$agelm[indexMissingAge]
-
-survivedlm<-lm(survived~pclass+sex+boat+parch+sibsp,data=train)
-
-
-##0.7
-all$survived<-as.factor(all$survived)
-all$pclass<-as.ordered(all$pclass)
-
-p1f<-which((all$pclass == "1") & (all$sex == "female"))
-p2f<-which((all$pclass == "2") & (all$sex == "female"))
-p3f<-which((all$pclass == "3") & (all$sex == "female"))
-p1m<-which((all$pclass == "1") & (all$sex == "male"))
-p2m<-which((all$pclass == "2") & (all$sex == "male"))
-p3m<-which((all$pclass == "3") & (all$sex == "male"))
-all$pclass_sex[p1f]="p1f"
-all$pclass_sex[p2f]="p2f"
-all$pclass_sex[p3f]="p3f"
-all$pclass_sex[p1m]="p1m"
-all$pclass_sex[p2m]="p2m"
-all$pclass_sex[p3m]="p3m"
-as.factor(all$pclass_sex)
+all<-rbind(raw_data,quest)
 
 all$boat = as.numeric(all$boat != "")
-agelm <- lm(age ~ pclass+boat+sex+parch+sibsp, data=all[!is.na(train$age),])
+agelm <- lm(age ~ pclass+sex+parch+sibsp+boat, data=all[!is.na(all$age),])
 summary(agelm)
 all$agelm<-predict(agelm,all)
-
 hist(all$age[!is.na(all$age)], main='Original data, non-missing', xlab='age')
 hist(all$agelm, main= 'LM predictions', xlab='age', xlim=range(0:80))
-
 indexMissingAge <- which(is.na(all$age))
-train$age[indexMissingAge] <- all$agelm[indexMissingAge]
+all$age[indexMissingAge] <- all$agelm[indexMissingAge]
+#透過曲線擬和，將age的missing data消除
 
-survivedLM<-lm(survived~pclass+boat+sex,parch)
+fare_NAindex = which(is.na(all$fare))
+#發現持有3701號票根的客人在fare factor處有na
+temp = all$fare[nchar( as.character(all$ticket)) == 4 & grepl("^3",as.character(all$ticket))]
+average = sum(temp[!is.na(temp)])/(length(temp)-1)
+all[fare_NAindex,"fare"] = average
+#將類似票根號的票價取平均
 
+#將資料拆分成四個區塊
+female_insea = all[all$sex == "female" & all$boat == 0,c("age","fare","survived")]
+female_onboat = all[all$sex == "female" & all$boat == 1,c("age","fare","survived")]
+male_insea = all[all$sex == "male" & all$boat == 0,c("age","fare","survived")]
+male_onboat = all[all$sex == "male" & all$boat == 1,c("age","fare","survived")]
 
+female_insea[,3] = as.factor(female_insea[,3])
+female_onboat[,3] = as.factor(female_onboat[,3])
+male_insea[,3] = as.factor(male_insea[,3])
+male_onboat[,3] = as.factor(male_onboat[,3])
 
-###0.8 PCA 
-library(factoextra)
-titanic_train<-all[]
-res.pca <- prcomp(train)
+#對fare取log10
+female_insea[,2] = log10(female_insea[,2])
+female_onboat[,2] = as.factor(female_onboat[,2])
+male_insea[,2] = as.factor(male_insea[,2])
+male_onboat[,2] = as.factor(male_onboat[,2])
+
+#分別對每個區塊做SVM
+library(e1071)
+plot_svm <- function(dat){
+  survived = as.numeric(dat[,3])
+  train_index = !is.na(survived)
+  svmfit = svm(survived[train_index] ~ ., data = dat[train_index,],gamma = 1, cost = 100)
+  return(svmfit)
+}
+par(mfrow=c(2,2))
+
+svm1 = plot_svm(female_insea)
+#plot(temp,female_insea[!is.na(female_insea$survived),])
+svm2 = plot_svm(female_onboat)
+# plot(temp,female_onboat[!is.na(female_onboat$survived),])
+svm3 = plot_svm(male_insea)
+# plot(temp,male_insea[!is.na(male_insea$survived),])
+svm4 = plot_svm(male_onboat)
+# plot(temp,male_onboat[!is.na(male_onboat$survived),])
+
+outcome1 = data.frame(predict(svm1,female_insea[is.na(female_insea$survived),c(1:2)]))
+outcome2 = data.frame(predict(svm2,female_onboat[is.na(female_onboat$survived),c(1:2)]))
+outcome3 = data.frame(predict(svm3,male_insea[is.na(male_insea$survived),c(1:2)]))
+outcome4 = data.frame(predict(svm4,male_onboat[is.na(male_onboat$survived),c(1:2)]))
+
+#合併結果進行輸出
+names(outcome1) = "y"
+names(outcome2) = "y"
+names(outcome3) = "y"
+names(outcome4) = "y"
+
+ans = rbind(outcome1,outcome2,outcome3,outcome4)
+index = order(row.names(ans))
+ans = ans[index,]
+write.csv(ans,file="titanicPrediction.csv")
